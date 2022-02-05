ec2077c9d5cfd9a5803085742c3624da998e3c01
aezi <30462422+minichest@users.noreply.github.com>
2022-02-05 14:52:32 +0000

    Update randomUrl.js

--- scripts/randomUrl.js
+++ scripts/randomUrl.js
@@ -1,11 +1,11 @@
 var randomUrl = [
-    "https://google.com",
-    "https://jobless.com",
-    "https://urfinished.com",
-    "https://mancity.com",
-    "https://github.com/minichest/Chapter-1",
-    "https://aezlo.com/Chapter-2",
-    "https://www.latlmes.com/opinion/teenagers-spend-their-time-using-their-phones-is-it-worth-it-1"
+    'https://google.com',
+    'https://jobless.com',
+    'https://urfinished.com',
+    'https://mancity.com',
+    'https://github.com/minichest/Chapter-1',
+    'https://aezlo.com/Chapter-2',
+    'https://www.latlmes.com/opinion/teenagers-spend-their-time-using-their-phones-is-it-worth-it-1'
 ]
 
 var randomNumber = (Math.floor(Math.random * (randomUrl.length)))
